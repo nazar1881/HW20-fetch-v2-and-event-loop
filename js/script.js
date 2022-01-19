5a3ff051a0768bd9631b0cfd26157859d5fce568
nazar1881 <90273320+nazar1881@users.noreply.github.com>
2022-01-19 13:18:21 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,94 @@
+let postUrl = 'https://jsonplaceholder.typicode.com/posts';
+let title = document.getElementById('title');
+let description = document.getElementById('description');
+let listOfComments = document.getElementById('coments-list');
+let writeComment = document.getElementById('input-comment');
+let addCommentBtn = document.getElementById('add-comment-btn');
+
+class Post {
+    constructor(postTitle, postDescription, postComments, id) {
+        this.postTitle = postTitle;
+        this.postDescription = postDescription;
+        this.postComments = postComments;  
+        this.id = id;
+    }
+
+    async getPost() {
+        try {
+            let response = await fetch(`${postUrl}/${this.id}`);
+            return response.json();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async showPost() {
+        try {
+            let data = await this.getPost();
+            this.renderPost(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    renderPost(post) {
+        this.postTitle.innerHTML = `<h3>Title: ${post.title}</h3>`;
+        this.postDescription.innerHTML = `<p><strong>Description:</strong> ${post.body}</p>`;
+    }
+
+    async getComments() {
+        try {
+            let response = await fetch(`${postUrl}/${this.id}/comments`);
+            return response.json();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async showComments() {
+        try {
+            let data = await this.getComments();
+            this.renderComments(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    renderComments(comments) {
+        let commentsList = "";
+        for (const el of comments) {
+            if (!el) {
+                return;
+            }
+            commentsList += `<li class="comments-item">${el.body}</li>`;
+        }
+        this.postComments.innerHTML = commentsList;
+    }
+}
+
+let post1 = new Post(title, description, listOfComments, 1);
+
+post1.showPost();
+post1.showComments();
+
+//////////////////////////Second task/////////////////////////////
+
+console.log(1);
+
+setTimeout(function () {
+    console.log(2);
+}, 100);
+
+setTimeout(function () {
+    console.log(3);
+}, 0);
+
+new Promise(function (resolve) {
+    setTimeout(function () {
+        resolve()
+    }, 0);
+}).then(() => {
+    console.log(4);
+});
+
+console.log(5);
